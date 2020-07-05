bc28f9c566800433883eeed8542d48a98120cb10
Thais Durynek <thais.durynek@gmail.com>
2020-07-05 19:48:44 -0300

    :construction: função mdLinks retornando objeto com text, href e file

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+
+function mdLinks(path) {
+  let regEx = /\[(\S.*)\]\((https*.*)\)/g;
+  //return new Promise((resolve,reject) => {
+  fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    if (data) {
+      const dataArray = data.match(regEx);
+      dataArray.map((link) => {
+        const result = {
+          href: link.match(/\((http.*)\)/)[1],
+          text: link.match(/\[(.*)\]/)[1],
+          file: path
+        }
+        return console.log(result);
+      });
+      //Promise.all(result).then(() => console.log(resolve(result)));
+    }
+  })
+  //})    
+}
+
+mdLinks('./test.md');
+
+// module.exports = mdLinks;
---
+++ test.md
@@ -0,0 +1,10 @@
+# Introdução
+
+Arquivo markdown teste. Aqui está um link para o que é markdown: 
+
+[Markdown](https://pt.wikipedia.org/wiki/Markdown)
+
+Aqui está um link para o meu linkedin: [Thais Durynek](https://www.linkedin.com/in/thaisdurynek/).
+
+* [ ] O coronavírus irá destruir tudo, caramba.
+* [ ] [(Teste unitário.)](https://jestjs.io/docs/pt-BR/getting-started)

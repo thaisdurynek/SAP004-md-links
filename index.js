f1dd714448947aca0f8d7bd727cb7fb2cc205dda
Thais Durynek <thais.durynek@gmail.com>
2020-07-06 12:19:01 -0300

    :construction: mdLinks fornecendo uma promisse

--- index.js
+++ index.js
@@ -2,25 +2,30 @@ const fs = require('fs');
 
 function mdLinks(path) {
   let regEx = /\[(\S.*)\]\((https*.*)\)/g;
-  //return new Promise((resolve,reject) => {
-  fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      console.log(err);
-    }
-    if (data) {
-      const dataArray = data.match(regEx);
-      dataArray.map((link) => {
-        const result = {
-          href: link.match(/\((http.*)\)/)[1],
-          text: link.match(/\[(.*)\]/)[1],
-          file: path
-        }
-        return console.log(result);
-      });
-      //Promise.all(result).then(() => console.log(resolve(result)));
-    }
+  let promise = new Promise((resolve,reject) => {
+    fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
+      if (err) {
+        reject(err.message);
+      }
+      else if (data) {
+        let arr = [];
+        const dataArray = data.match(regEx);
+        dataArray.map((link) => {
+          const href = link.match(/\((https*.*)\)/)[1];
+          const text = link.match(/\[(\S.*)\]/)[1];
+          arr.push({
+            href: href,
+            text: text,
+            file: path
+          })
+        });
+        return resolve(arr);
+      }
+    })
   })
-  //})    
+  promise.then((result) => {
+    console.log(result);
+  })   
 }
 
 mdLinks('./test.md');
